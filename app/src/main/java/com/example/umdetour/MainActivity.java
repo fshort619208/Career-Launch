4f2eac16d8d7b2d27ff0fcc39e427615797b689b
Angela Zhang <angelazhang915@gmail.com>
2024-01-18 19:29:48 -0500

    first commit

---
+++ app/src/main/java/com/example/umdetour/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.umdetour;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+        LatLng umd = new LatLng(39, -77);
+        myMap.addMarker(new MarkerOptions().position(umd).title("UMD"));
+
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(umd));
+    }
+}
